7ddadc2b7a3268ae9bb274fd568a82c985a7cd28
bunvin <37735956+bunvin@users.noreply.github.com>
2024-04-02 20:04:59 +0000

    clean up, clear comments

--- main.py
+++ main.py
@@ -3,30 +3,56 @@
 # pick an operator
 # input another number
 # get the result
-#would you like to continue ("yes" or "no")?
+# would you like to continue ("yes" or "no")? 
+#### 'yes' another operation, 'no' new calculation
 
-
+#operation functions
 def add(n1, n2):
   return n1 + n2
-
 
 def subtract(n1, n2):
   return n1 - n2
 
-
 def multiply(n1, n2):
   return n1 * n2
-
 
 def divide(n1, n2):
   return n1 / n2
 
+#dictionary of operations
+operations = {
+  "+": add, 
+  "-": subtract, 
+  "*": multiply, 
+  "/": divide
+}
 
-operations = {"+": add, "-": subtract, "*": multiply, "/": divide}
+#calculator fuction call itself to restart the calculator
+def calculator():
+  num1 = int(input("What's the number?: "))
+  
+  for symbol in operations:
+    print(symbol)
+  
+  should_continue = True
+  
+  while should_continue:
+    
+    operation_symbol = input("Pick an operation: ")
+    
+    num2 = int(input("What's the next number?: "))
+    
+    #calling and using the function from dictionary
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+    
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+  
+    continue_calculation = input("type 'y' to continue or type 'n' to start a new calculation")
+    if continue_calculation == "y":
+      num1 = answer
+    else:
+      should_continue = False
+      calculator()
 
-#calling the function
-function = operations["*"]
-#using the function
-print(function(2, 3))
-
-num1 = int(input("What's the first number?: "))
+calculator()

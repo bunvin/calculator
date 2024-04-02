71592cedcac55d4156e37497b35bcae983095a41
bunvin <37735956+bunvin@users.noreply.github.com>
2024-04-02 13:47:56 +0000

    calculator file

--- main.py
+++ main.py
@@ -3,28 +3,30 @@
 # pick an operator
 # input another number
 # get the result
-#would you like to continue ("yes" or "no")? 
+#would you like to continue ("yes" or "no")?
+
 
 def add(n1, n2):
-  return n1+ n2
+  return n1 + n2
+
 
 def subtract(n1, n2):
   return n1 - n2
 
+
 def multiply(n1, n2):
   return n1 * n2
+
 
 def divide(n1, n2):
   return n1 / n2
 
-operations = {
-  "+": add,
-  "-": subtract,
-  "*": multiply,
-  "/": divide
-}
+
+operations = {"+": add, "-": subtract, "*": multiply, "/": divide}
 
 #calling the function
 function = operations["*"]
 #using the function
-print(function(2,3))
+print(function(2, 3))
+
+num1 = int(input("What's the first number?: "))
